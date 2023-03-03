76c4fa2bdf01fe45aba0436ebe2e5737567b5158
Andrew Chou <andrewchou1996@gmail.com>
2023-03-03 14:38:48 -0800

    Add project 2

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 let projectOneEl = document.querySelector("#project01");
 let projectOneUrl = "https://jlddukes.github.io/cocktail-generator/";
 
-projectOneEl.addEventListener("click", function (e) {
-    e.preventDefault();
+let projectTwoEl = document.querySelector("#project02");
+let projectTwoUrl = "https://boiling-dusk-37737.herokuapp.com/";
 
-    window.open(projectOneUrl, '_blank');
-})
+projectOneEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.open(projectOneUrl, "_blank");
+});
+
+projectTwoEl.addEventListener("click", function (e) {
+  e.preventDefault();
+  window.open(projectTwoUrl, "_blank");
+});
